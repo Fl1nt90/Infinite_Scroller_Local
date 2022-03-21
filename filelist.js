b5de8d27423d95c422efa84a3fe9d081cc34a2de
Fl1nt90 <abbaccacacca@gmail.com>
2022-03-21 15:58:20 +0100

    randomize ok

--- filelist.js
+++ filelist.js
@@ -1,21 +1,17 @@
 const state = {
   reversed: false,
   initial: [
-    "img/images/103573552_939443276487246_5541652549148429467_n.jpg",
-    "img/images/137621466_10161259427131840_625118455530440663_o.jpg",
-    "img/images/agat.jpg",
-    "img/images/glass.jpg",
-    "img/images/Senza titolo-1.jpg",
-    "img/images/Senza titolo-1sa.jpg",
-    "img/images/Senza titolo-2.jpg",
-    "img/images/Senza titolo-3.jpg",
-    "img/images/Senza titolo-14.jpg",
+    'img/images/103573552_939443276487246_5541652549148429467_n.jpg', 'img/images/137621466_10161259427131840_625118455530440663_o.jpg', 'img/images/238721187_4687937251228091_6662378667257854032_n.jpg', 'img/images/240823600_6102053639869842_8744533995202412948_n.jpg', 'img/images/265960819_6679560892119111_6618449473800202224_n.jpg', 'img/images/agat.jpg', 'img/images/c8a75cb4ca940e1d570d7f027299deba9bdfa7d71e6d8290ac7be086eeca0b05.jpg', 'img/images/glass.jpg', 'img/images/Senza titolo-1.jpg', 'img/images/Senza titolo-1sa.jpg', 'img/images/Senza titolo-2.jpg', 'img/images/Senza titolo-3.jpg', 'img/images/Senza titolo-14.jpg'
   ],
 };
 
 export let tempPhotoArray = state.initial; //initial state
+ 
 
 export const reverse = function () {
+  console.log(tempPhotoArray);
+  console.log(tempPhotoArray, sada,);
+
   //function to reverse the order of the photo
   if (!state.reversed) {
     tempPhotoArray = []; //empty the array
--- script.js
+++ script.js
@@ -6,6 +6,8 @@ import {
 } from "./filelist.js";
 
 
+
+
 const title = document.querySelector("h1");
 const uploadContainer = document.getElementById("upload");
 const buttonsContainer = document.querySelector(".buttons");
@@ -13,88 +15,77 @@ const imageContainer = document.querySelector(".image-container");
 const loader = document.getElementById("loader");
 const goButton = document.getElementById("go");
 
-let showUpload = false;
-
 const count = 3; //image to load every scroll
-let readyBoolean = false; //variable to check if all images are loaded
 let imagesLoaded = 0;
 let totalImages = 0; //to keep track of the total images
 
-let imagesDisplayed = []; //need the global array to later sort/randomize
+let showUpload = false; //to show the title upload box
+let readyBoolean = false; //variable to check if all images are loaded
+//ready boleand to momentary disable the "scroll" event while rendering randomized/sorted elements
+let disableScroll = false; 
+
+let imagesDisplayed = []; //need the global array to sort/randomize
+
 
 
 //FUNCTION FIRED AFTER EVERY IMAGE IS LOADED
-const loaded = function () {
-  imagesLoaded++; //every loaded image, increase the counter by 1
+const loaded = function (randomizing) {
+  if (!randomizing) imagesLoaded++; //every loaded image, increase the counter by 1
   console.log(imagesLoaded, totalImages);
+  console.log(randomizing);
 
-  if (imagesLoaded === totalImages) {
-    readyBoolean = true; //se the ready boolean to TRUE
+  //WHEN ALL IMAGES ARE LOADED
+  if (imagesLoaded === totalImages) { 
     loader.hidden = true; //hide the loader
+    readyBoolean = true; //se the ready boolean to TRUE
+    disableScroll = false; //re-enable the "scroll" event listener
   }
 };
 
 //DISPLAY THE PHOTO ON THE PAGE
-const displayPhotos = function (photos) {
+const displayPhotos = function (photos, randomizing = false) {
   //loop over the photos array and create element for every object in the array
   photos.forEach((element) => {
     const img = document.createElement("img"); //create image element
     img.setAttribute("src", element); //set the source
     imageContainer.appendChild(img); //insert in the page
+
     //event listener for load event for every image
     img.addEventListener("load", function () {
-      loaded();
+      loaded(randomizing);
     });
+    
   });
+  
 };
 
 //GET AND SLICE THE PHOTOS
-const getPhotos = function () {
-    const slicePhotosArray = tempPhotoArray.slice(imagesLoaded, imagesLoaded + count)
-    imagesDisplayed.push(slicePhotosArray); //push to the global array
-    console.log(slicePhotosArray, imagesDisplayed);
-    totalImages += slicePhotosArray.length; 
-    displayPhotos(slicePhotosArray);
+const getSplicePhotos = function () {
+    const afterSplice = tempPhotoArray.splice(0, count);
+    imagesDisplayed.push(...afterSplice); //push to the global array
+    totalImages += afterSplice.length; 
+    displayPhotos(afterSplice);
 };
-getPhotos(); //immediately call at run-time
+getSplicePhotos(); //immediately call at run-time
 
 
-// //get photos, slice and show on the page
-// const getSliceDisplayPhotos = function () {
-//   //slice the array, push into the displayed images and set the totalImages variable
-//     const slicePhotosArray = tempPhotoArray.slice(imagesLoaded, imagesLoaded + count)
-//     imagesDisplayed.push(slicePhotosArray); //push to the global array
-//     totalImages += slicePhotosArray.length; 
-
-//   //display the images on the page, looping over the scliced array
-//     slicePhotosArray.forEach((element) => {
-//     const img = document.createElement("img"); //create image element
-//     img.setAttribute("src", element); //set the source
-//     imageContainer.appendChild(img); //insert in the page
-
-//   //event listener for load event for every image
-//     img.addEventListener("load", function () {
-//       loaded(); //call the loaded function
-//     });
-//   });
-// };
-// getSliceDisplayPhotos();
 
 
 //CLEAN THE PHOTOS IN THE IMAGE CONTAINER /remove childre/remove all the child/delete children
 function removeAllChildNodes(parent) {
+  disableScroll = true; //disable the scroll before updating the images
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
 }
 
 //SCROLL EVENT LISTENER
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function() {
   if (
-    window.innerHeight + window.scrollY > document.body.offsetHeight -300 &&
-    readyBoolean
+    window.innerHeight + window.scrollY > document.body.offsetHeight -200 &&
+    readyBoolean && (!disableScroll)
   ) {
-    getPhotos();
+    getSplicePhotos();
     readyBoolean = false; //reset the readyBoolean variable after loading new images
   }
 });
@@ -111,7 +102,6 @@ title.addEventListener("click", function () {
     showUpload = false;
   }
 });
-
 //call the function to create the array with html
 goButton.addEventListener("click", function () {
   go(); //call the function to process the images and copy the HTML output to clipboard
@@ -121,12 +111,14 @@ goButton.addEventListener("click", function () {
 
 //SORT and RANDOMIZE BUTTONS EVENT LISTENER
 buttonsContainer.addEventListener("click", function (e) {
-  e.preventDefault();
-
   if (!e.target.id) return;
   removeAllChildNodes(imageContainer);
-  if (e.target.id === "randomize") randomize(imagesDisplayed);
+
+  if (e.target.id === "randomize") {
+    randomize(imagesDisplayed);
+  }
   if (e.target.id === "sort") reverse();
 
-  displayPhotos(imagesDisplayed);
+  displayPhotos(imagesDisplayed, true);
 });
+
